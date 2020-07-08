f2a1ae0f2f6b3918fda6a603cb3b471a2a966e3b
gerardogobti <59845216+gerardogobti@users.noreply.github.com>
2020-07-08 14:19:02 -0500

    PRUEAB

--- prueba.php
+++ prueba.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VSCode</title>
+    <title>zxzxVSCode2 mas cambios registrados</title>
 </head>
 <body>
     
