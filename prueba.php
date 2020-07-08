f1ef4795ab46381093915f1437cacacec609e9db
gerardogobti <59845216+gerardogobti@users.noreply.github.com>
2020-07-08 13:51:15 -0500

    inicial commit

---
+++ prueba.php
@@ -0,0 +1,12 @@
+// ejemplo de git para visual estudiocode
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VSCode</title>
+</head>
+<body>
+    
+</body>
+</html>
